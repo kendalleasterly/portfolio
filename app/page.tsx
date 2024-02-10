6999cdc03316dfba54c8797104188ea66386b34a
Kendall Easterly <qfletcher89@gmail.com>
2024-02-09 23:32:32 -0600

    did stuff

--- app/page.tsx
+++ app/page.tsx
@@ -38,11 +38,11 @@ export default function Home() {
           I'm building{" "}
           <Link
             target="_blank"
-            href="https://unkey.dev"
+            href="https://kendalleasterly.com/projects/budgeting"
             className="underline duration-500 hover:text-zinc-300"
           >
-            unkey.dev
-          </Link> to solve API authentication and authorization for developers.
+            FlexiTrackr
+          </Link> to make budgeting easy yet fully customizable for anyone
         </h2>
       </div>
     </div>
